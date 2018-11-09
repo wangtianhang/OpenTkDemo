5e143e87464608809c1d30338f4bc16455408669
wangtianhang <690879430@qq.com>
2018-11-09 14:33:51 +0800

    添加部分fake脚本

---
+++ testOpenTk2/unity/GameObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityEngine
+{
+
+}
+
+public class Component
+{
+
+}
+
+public class Behaviour : Component
+{
+
+}
+
+public class MonoBehaviour : Behaviour
+{
+
+}
+
+
+public class GameObject
+{
+    Transform m_transform = new Transform();
+    List<Component> m_componentList = new List<Component>();
+
+    public Transform transform
+    {
+        get 
+        {
+            return m_transform;
+        }
+    }
+
+    public MeshFilter meshFilter
+    {
+        get
+        {
+            return GetComponent<MeshFilter>();
+        }
+    }
+
+    public T AddComponent<T>() where T : Component
+    {
+        T ret = default(T);
+        m_componentList.Add(ret);
+        return ret;
+    }
+
+    public T GetComponent<T>() where T : Component
+    {
+        foreach(var iter in m_componentList)
+        {
+            if(iter is T)
+            {
+                return iter as T;
+            }
+        }
+
+        return null;
+    }
+}
+
---
+++ testOpenTk2/unity/Transform.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// 坐标系与unity保持一致 为左手坐标系
+/// </summary>
+
+public class Transform : Component
+{
+    Vector3 m_position = Vector3.zero;
+
+    //Vector3 m_localScale;
+
+    Quaternion m_rotation = Quaternion.identity;
+
+    Vector3 m_scale = Vector3.one;
+
+    Transform m_parent = null;
+
+    public Transform parent
+    {
+        get
+        {
+            return m_parent;
+        }
+    }
+
+    public Matrix4x4 localToWorldMatrix
+    {
+        get 
+        {
+            return Matrix4x4.TRS(m_position, m_rotation, m_scale);
+        }
+    }
+
+    // 旋转转朝向
+//     public static Vector3 GetForward(Quaternion rotation)
+//     {
+//         return rotation * Vector3.forward;
+//     }
+
+    // 朝向转旋转
+//     public static Quaternion LookAt(Vector3 dir)
+//     {
+//         return Quaternion.LookRotation(dir, Vector3.up);
+//     }
+}
+
