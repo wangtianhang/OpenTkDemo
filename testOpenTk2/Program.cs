96d20972a20573506f81ceaddfdacba256b2e4da
wangtianhang <690879430@qq.com>
2018-11-09 10:16:12 +0800

    添加console

--- testOpenTk2/Program.cs
+++ testOpenTk2/Program.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace testOpenTk2
 {
     static class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
         static void Main()
         {
+            AllocConsole();
+
             new MainWindow().Run(60);
         }
     }
