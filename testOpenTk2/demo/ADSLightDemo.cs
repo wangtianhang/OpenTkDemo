8358d2c4b0f53924ecbd3ff4e1be7309e51d9393
wangtianhang <690879430@qq.com>
2018-11-15 19:35:34 +0800

    补提交

--- testOpenTk2/demo/ADSLightDemo.cs
+++ testOpenTk2/demo/ADSLightDemo.cs
@@ -8,9 +8,90 @@ using OpenTK.Graphics.ES30;
 
 class ADSLightDemo : IDemo
 {
+    string vShaderStr = @"
+// ADS Point lighting Shader
+// Vertex Shader
+// Richard S. Wright Jr.
+// OpenGL SuperBible
+#version 300 es
+precision highp float;
+
+// Incoming per vertex... position and normal
+layout(location = 0) in vec4 vVertex;
+layout(location = 1)in vec3 vNormal;
+
+
+uniform mat4   mvpMatrix;
+uniform mat4   mvMatrix;
+uniform mat3   normalMatrix;
+uniform vec3   vLightPosition;
+
+// Color to fragment program
+smooth out vec3 vVaryingNormal;
+smooth out vec3 vVaryingLightDir;
+
+void main(void) 
+    { 
+    // Get surface normal in eye coordinates
+    vVaryingNormal = normalMatrix * vNormal;
+
+    // Get vertex position in eye coordinates
+    vec4 vPosition4 = mvMatrix * vVertex;
+    vec3 vPosition3 = vPosition4.xyz / vPosition4.w;
+
+    // Get vector to light source
+    vVaryingLightDir = normalize(vLightPosition - vPosition3);
+
+
+    // Don't forget to transform the geometry!
+    gl_Position = mvpMatrix * vVertex;
+    }
+";
+
+    string fShaderStr = @"
+// ADS Point lighting Shader
+// Fragment Shader
+// Richard S. Wright Jr.
+// OpenGL SuperBible
+#version 300 es
+precision highp float;
+
+out vec4 vFragColor;
+
+uniform vec4    ambientColor;
+uniform vec4    diffuseColor;   
+uniform vec4    specularColor;
+
+smooth in vec3 vVaryingNormal;
+smooth in vec3 vVaryingLightDir;
+
+
+void main(void)
+    { 
+    // Dot product gives us diffuse intensity
+    float diff = max(0.0, dot(normalize(vVaryingNormal), normalize(vVaryingLightDir)));
+
+    // Multiply intensity by diffuse color, force alpha to 1.0
+    vFragColor = diff * diffuseColor;
+
+    // Add in ambient light
+    vFragColor += ambientColor;
+
+
+    // Specular Light
+    vec3 vReflection = normalize(reflect(-normalize(vVaryingLightDir), normalize(vVaryingNormal)));
+    float spec = max(0.0, dot(normalize(vVaryingNormal), vReflection));
+    if(diff != 0f) {
+        float fSpec = pow(spec, 128.0);
+        vFragColor.rgb += vec3(fSpec, fSpec, fSpec);
+        }
+    }
+";
+    public int m_program;
+
     public void Init(MainWindow mainWindow)
     {
-
+        m_program = OpenGLMgr._CompilerShader(vShaderStr, fShaderStr);
     }
 
     public void OnUpdateFrame(FrameEventArgs e)
