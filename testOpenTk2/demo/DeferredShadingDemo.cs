e58cd3bf4579d34ceb97f42ff00944cc1cea2261
wth <690879430@qq.com>
2018-11-18 19:53:54 +0800

    部分deferred shader

--- testOpenTk2/demo/DeferredShadingDemo.cs
+++ testOpenTk2/demo/DeferredShadingDemo.cs
@@ -3,14 +3,135 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using OpenTK;
+using OpenTK;
 using OpenTK.Graphics.ES30;
 
 class DeferredShadingDemo : IDemo
 {
+    public string prePassVShader = @"
+#version 300 es
+precision highp float;
+
+// Incoming per vertex... position and normal
+layout(location = 0) in vec4 vVertex;
+layout(location = 1) in vec3 vNormal;
+
+uniform mat4   mvpMatrix;
+uniform mat4   mvMatrix;
+uniform vec3   vLightPosition;
+
+out vec3 vVaryingNormal;
+out vec3 vVaryingLightDir;
+
+void main(void) 
+    { 
+// Get surface normal in eye coordinates
+    //mat3 normalMatrix = mvMatrix;
+    vec4 normalWrap = vec4(vNormal.x, vNormal.y, vNormal.z, 0);
+    vVaryingNormal = (mvMatrix * normalWrap).xyz;
+
+    // Get vertex position in eye coordinates
+    vec4 vPosition4 = mvMatrix * vVertex;
+    vec3 vPosition3 = vPosition4.xyz / vPosition4.w;
+
+    // Get vector to light source
+    vVaryingLightDir = normalize(vLightPosition - vPosition3);
+
+    gl_Position = mvpMatrix * vVertex;
+
+    }
+";
+    public string prePassFShader = @"
+#version 300 es
+precision highp float;
+
+uniform vec4    ambientColor;
+uniform vec4    diffuseColor; 
+uniform vec4 specularColor;
+
+in vec3 vVaryingNormal;
+in vec3 vVaryingLightDir;
+
+layout(location = 0) out vec4 color0;
+layout(location = 1) out vec4 color1;
+
+void main(void)
+    {
+        color0 = vec4(vVaryingNormal, 1);
+        color1 = vec4(vVaryingLightDir, 1);
+    }
+";
+    public string lightPassVShader = @"
+#version 300 es
+precision highp float;
+
+void main(void)
+{
+    const vec4 verts[4] = vec4[4](vec4(-1.0, -1.0, 0.5, 1.0),
+                                  vec4( 1.0, -1.0, 0.5, 1.0),
+                                  vec4(-1.0,  1.0, 0.5, 1.0),
+                                  vec4( 1.0,  1.0, 0.5, 1.0));
+
+    gl_Position = verts[gl_VertexID];
+}
+";
+
+    public string lightPassFShader = @"
+#version 300 es
+precision highp float;
+
+layout (location = 0) out vec4 vFragColor;
+
+uniform vec4    ambientColor;
+uniform vec4    diffuseColor; 
+
+uniform sampler2D gbuf_tex0;
+uniform sampler2D gbuf_tex1;
+
+void main(void)
+{
+    ivec2 coord = ivec2(gl_FragCoord.xy);
+    vec4 data0 = texelFetch(gbuf_tex0, ivec2(coord), 0);
+    vec4 data1 = texelFetch(gbuf_tex1, ivec2(coord), 0);
+
+    vec3 vVaryingNormal = data0.xyz;
+    vec3 vVaryingLightDir = data1.xyz;
+    
+            // Dot product gives us diffuse intensity
+    float diff = max(0.0, dot(normalize(vVaryingNormal), normalize(vVaryingLightDir)));
+
+    // Multiply intensity by diffuse color, force alpha to 1.0
+    vFragColor = diff * diffuseColor;
+
+    // Add in ambient light
+    vFragColor += ambientColor;
+
+
+    // Specular Light
+    vec3 vReflection = normalize(reflect(-normalize(vVaryingLightDir), normalize(vVaryingNormal)));
+    float spec = max(0.0, dot(normalize(vVaryingNormal), vReflection));
+    if(diff != 0.0f) 
+    {
+        float fSpec = pow(spec, 128.0);
+        vFragColor.rgb += vec3(fSpec, fSpec, fSpec);
+    }
+}
+
+";
+
+    public class MeshData
+    {
+        public float[] m_data;
+        public ushort[] m_index;
+    }
+
+    public int m_prePassProgram;
+    public int m_lightPassProgram;
+
     public void Init(MainWindow mainWindow)
     {
-
+        m_prePassProgram = OpenGLMgr._CompilerShader(prePassVShader, prePassFShader);
+        m_lightPassProgram = OpenGLMgr._CompilerShader(lightPassVShader, lightPassFShader);
     }
 
     public void OnUpdateFrame(OpenTK.FrameEventArgs e)
@@ -21,6 +142,118 @@ class DeferredShadingDemo : IDemo
     public void OnRenderFrame(OpenTK.FrameEventArgs e)
     {
 
+    }
+
+    MeshData GetCubeMesh()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        vertices.Add(new Vector3(0.5f, -0.5f, 0.5f));
+        vertices.Add(new Vector3(-0.5f, -0.5f, 0.5f));
+        vertices.Add(new Vector3(0.5f, 0.5f, 0.5f));
+        vertices.Add(new Vector3(-0.5f, 0.5f, 0.5f));
+        vertices.Add(new Vector3(0.5f, 0.5f, -0.5f));
+        vertices.Add(new Vector3(-0.5f, 0.5f, -0.5f));
+        vertices.Add(new Vector3(0.5f, -0.5f, -0.5f));
+        vertices.Add(new Vector3(-0.5f, -0.5f, -0.5f));
+        vertices.Add(new Vector3(0.5f, 0.5f, 0.5f));
+        vertices.Add(new Vector3(-0.5f, 0.5f, 0.5f));
+        vertices.Add(new Vector3(0.5f, 0.5f, -0.5f));
+        vertices.Add(new Vector3(-0.5f, 0.5f, -0.5f));
+        vertices.Add(new Vector3(0.5f, -0.5f, -0.5f));
+        vertices.Add(new Vector3(0.5f, -0.5f, 0.5f));
+        vertices.Add(new Vector3(-0.5f, -0.5f, 0.5f));
+        vertices.Add(new Vector3(-0.5f, -0.5f, -0.5f));
+        vertices.Add(new Vector3(-0.5f, -0.5f, 0.5f));
+        vertices.Add(new Vector3(-0.5f, 0.5f, 0.5f));
+        vertices.Add(new Vector3(-0.5f, 0.5f, -0.5f));
+        vertices.Add(new Vector3(-0.5f, -0.5f, -0.5f));
+        vertices.Add(new Vector3(0.5f, -0.5f, -0.5f));
+        vertices.Add(new Vector3(0.5f, 0.5f, -0.5f));
+        vertices.Add(new Vector3(0.5f, 0.5f, 0.5f));
+        vertices.Add(new Vector3(0.5f, -0.5f, 0.5f));
+
+
+        List<ushort> index = new List<ushort>();
+        index.Add(0);
+        index.Add(2);
+        index.Add(3);
+        index.Add(0);
+        index.Add(3);
+        index.Add(1);
+        index.Add(8);
+        index.Add(4);
+        index.Add(5);
+        index.Add(8);
+        index.Add(5);
+        index.Add(9);
+        index.Add(10);
+        index.Add(6);
+        index.Add(7);
+        index.Add(10);
+        index.Add(7);
+        index.Add(11);
+        index.Add(12);
+        index.Add(13);
+        index.Add(14);
+        index.Add(12);
+        index.Add(14);
+        index.Add(15);
+        index.Add(16);
+        index.Add(17);
+        index.Add(18);
+        index.Add(16);
+        index.Add(18);
+        index.Add(19);
+        index.Add(20);
+        index.Add(21);
+        index.Add(22);
+        index.Add(20);
+        index.Add(22);
+        index.Add(23);
+
+
+        List<Vector3> normals = new List<Vector3>();
+        normals.Add(new Vector3(0.0f, 0.0f, 1.0f));
+        normals.Add(new Vector3(0.0f, 0.0f, 1.0f));
+        normals.Add(new Vector3(0.0f, 0.0f, 1.0f));
+        normals.Add(new Vector3(0.0f, 0.0f, 1.0f));
+        normals.Add(new Vector3(0.0f, 1.0f, 0.0f));
+        normals.Add(new Vector3(0.0f, 1.0f, 0.0f));
+        normals.Add(new Vector3(0.0f, 0.0f, -1.0f));
+        normals.Add(new Vector3(0.0f, 0.0f, -1.0f));
+        normals.Add(new Vector3(0.0f, 1.0f, 0.0f));
+        normals.Add(new Vector3(0.0f, 1.0f, 0.0f));
+        normals.Add(new Vector3(0.0f, 0.0f, -1.0f));
+        normals.Add(new Vector3(0.0f, 0.0f, -1.0f));
+        normals.Add(new Vector3(0.0f, -1.0f, 0.0f));
+        normals.Add(new Vector3(0.0f, -1.0f, 0.0f));
+        normals.Add(new Vector3(0.0f, -1.0f, 0.0f));
+        normals.Add(new Vector3(0.0f, -1.0f, 0.0f));
+        normals.Add(new Vector3(-1.0f, 0.0f, 0.0f));
+        normals.Add(new Vector3(-1.0f, 0.0f, 0.0f));
+        normals.Add(new Vector3(-1.0f, 0.0f, 0.0f));
+        normals.Add(new Vector3(-1.0f, 0.0f, 0.0f));
+        normals.Add(new Vector3(1.0f, 0.0f, 0.0f));
+        normals.Add(new Vector3(1.0f, 0.0f, 0.0f));
+        normals.Add(new Vector3(1.0f, 0.0f, 0.0f));
+        normals.Add(new Vector3(1.0f, 0.0f, 0.0f));
+
+        MeshData meshData = new MeshData();
+        List<float> dataList = new List<float>();
+        for (int i = 0; i < vertices.Count; ++i)
+        {
+            Vector3 pos = vertices[i];
+            Vector3 normal = normals[i];
+            dataList.Add(pos.X);
+            dataList.Add(pos.Y);
+            dataList.Add(pos.Z);
+            dataList.Add(normal.X);
+            dataList.Add(normal.Y);
+            dataList.Add(normal.Z);
+        }
+        meshData.m_data = dataList.ToArray();
+        meshData.m_index = index.ToArray();
+        return meshData;
     }
 }
 
